d6ff78a95ef41d7637366370ccab3653ae250c61
peada3 <pranaveada@gatech.edu>
2025-12-30 12:39:34 -0500

    initialized firmware w simple test

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "regs.h"
+
+int main() {
+    //initializing hw_model 
+    hw_init();
+
+    //writing to a reg and reading value into variable (simple test 1)
+    hw_write_reg(0x20, 0x1234);
+    uint32_t read_value = hw_read_reg(0x20);
+
+    if (read_value == 0x1234) {
+        printf("PASS!\n");
+    } else {
+        printf("FAIL!\n");
+    }
+    return 0;
+}
+
